f7d55b7b454f05197f1c02602506ae9004572efb
Manav Misra <manavm@visionwebsoft.com>
2020-10-15 06:25:58 -0500

    :sparkles: Add solution 5

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,6 +1,16 @@
-const { extname } = require("path");
 const {
   promises: { readdir },
 } = require("fs");
+const { extname } = require("path");
 
-console.log(readdir, extname);
+const [_, __, dir, ext] = process.argv;
+
+(async () => {
+  const files = await readdir(dir);
+
+  files.forEach((file) => {
+    if (extname(file) === `.${ext}`) {
+      console.log(file);
+    }
+  });
+})();
