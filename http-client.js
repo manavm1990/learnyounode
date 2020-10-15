10110616680345e4ed70f209d53a4f79e5c49795
Manav Misra <manavm@visionwebsoft.com>
2020-10-15 10:55:23 -0500

    :sparkles: Add 'http-client' solution

--- http-client.js
+++ http-client.js
@@ -1,3 +1,10 @@
 const { get } = require("http");
 
-console.log(get);
+const [_, __, resource] = process.argv;
+
+get(resource, (res) => {
+  res.setEncoding("utf8");
+  res.on("data", (data) => {
+    console.info(data);
+  });
+});
